a112129d0d44e946d29cdbb4e8ed0d4b4fea83e0
Rob Sexmith <rsexmith@fuse.net>
2015-12-23 10:41:31 -0500

    Adding Makefile bitcask and start from make

---
+++ ebin/testOTP1.app
@@ -0,0 +1,8 @@
+{application,testOTP1,
+             [{description,[]},
+              {vsn,"1"},
+              {registered,[]},
+              {applications,[kernel,stdlib]},
+              {mod,{testOTP1_app,[]}},
+              {env,[]},
+              {modules,[testOTP1,testOTP1_app,testOTP1_sup]}]}.
--- src/testOTP1.erl
+++ src/testOTP1.erl
@@ -6,6 +6,11 @@
 start() ->
    io:format("~nHello my name is testOTP1~n"),
    timer:sleep(3000),
+    Handle = bitcask:open("testOTP1_database", [read_write]),
+    N = fetch(Handle),
+    store(Handle, N+1),
+    io:format("testOTP1 has been run ~p times~n",[N]),
+    bitcask:close(Handle),
    io:format("~nStarted Registered Name: hiya Spawned Process Receive Loop~n"),
    register(hiya, spawn(fun() -> loop() end)),
    timer:sleep(2000),
@@ -20,7 +25,20 @@ start() ->
      unregister(hiya)
  end,
    timer:sleep(5000),
-   io:format("TimeOut GoodBye from testOTP1.erl~n").
+   io:format("TimeOut GoodBye from testOTP1.erl~n"),
+   io:format("Doing init:stop~n"),
+   init:stop().
+
+
+store(Handle, N) ->
+    bitcask:put(Handle, <<"testOTP1_executions">>, term_to_binary(N)).  
+
+fetch(Handle) ->
+    case bitcask:get(Handle, <<"testOTP1_executions">>) of
+	not_found -> 1;
+	{ok, Bin} -> binary_to_term(Bin)
+    end.
+
 
 loop() ->
  receive
